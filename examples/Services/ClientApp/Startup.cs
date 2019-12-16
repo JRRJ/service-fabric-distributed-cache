b7f716cfcee7446221667d21e04ced08e2d29d29
John.Jenson <John.Jenson@SoCreate.it>
2019-12-16 11:11:35 -0800

    update packages to versions targeting Service Fabric 7.0 runtime

--- examples/Services/ClientApp/Startup.cs
+++ examples/Services/ClientApp/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace ClientApp
 {
@@ -20,11 +21,13 @@ namespace ClientApp
         {
             services.AddDistributedServiceFabricCache();
 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services
+                .AddMvc(options => options.EnableEndpointRouting = false)
+                .SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
